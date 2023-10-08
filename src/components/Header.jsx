ec5bedd823cec2e10e69fdbdd96c6ccdee6a01e3
gkartsivadze <qarcigio444@mail.ru>
2023-10-08 22:43:38 +0400

    - Setup finished

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header() {
+    return (
+        <nav>
+            <h1>Where in the world?</h1>
+            <button>
+                Dark Mode
+            </button>
+        </nav>
+    )
+};
