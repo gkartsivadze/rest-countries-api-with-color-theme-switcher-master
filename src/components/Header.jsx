1ef0d643829afca814645f23359fc1cadb099947
gkartsivadze <qarcigio444@mail.ru>
2023-10-09 10:45:46 +0400

    - finished mobile header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
+import { FaMoon } from 'react-icons/fa'
+
 export default function Header() {
     return (
-        <nav>
-            <h1>Where in the world?</h1>
-            <button>
-                Dark Mode
+        <nav className='flex justify-between px-4 py-6 border-b-[1px] border-dark-grey'>
+            <h1 className='font-extrabold'>Where in the world?</h1>
+            <button className=' font-semibold'>
+                <FaMoon className='inline me-2' /> Dark Mode
             </button>
         </nav>
     )
