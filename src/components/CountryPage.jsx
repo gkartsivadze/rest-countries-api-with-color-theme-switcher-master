a43db9fcedd685e676e75b35e7548fa90719f129
gkartsivadze <qarcigio444@mail.ru>
2023-10-09 11:20:08 +0400

    - react router set up - installed ant design

---
+++ src/components/CountryPage.jsx
@@ -0,0 +1,7 @@
+import { Link } from "react-router-dom";
+
+export default function CountryPage() {
+    return (
+        <Link to="/">Back</Link>
+    )
+};
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import Layout from './Layout.jsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Main from './components/Main.jsx'
+import CountryPage from './components/CountryPage.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <Main />
+      },{
+        path: '/country-page/:countryId',
+        element: <CountryPage />
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
